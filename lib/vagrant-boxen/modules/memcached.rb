e9917d9ff61ccfdc0b2485d1982f39080b942365
Fabio Rehm <fgrehm@gmail.com>
2013-02-20 00:06:57 -0300

    Add support for setting memcached max memory

--- lib/vagrant-boxen/modules/memcached.rb
+++ lib/vagrant-boxen/modules/memcached.rb
@@ -1,13 +1,20 @@
 module Vagrant::Boxen::Modules
   class Memcached
     module Config
-      def memcached!
-        enable_module(Memcached.new)
+      def memcached(options = nil, &options_block)
+        enable_module Memcached.new(options, &options_block)
       end
     end
 
+    include Vagrant::Boxen::BaseModule
+
+    def initialize(options = nil, &options_block)
+      options = build_options([:memory], options, &options_block)
+      @options = { :max_memory => options.fetch(:memory, '25') }
+    end
+
     def build_manifest
-      "class { 'memcached': }"
+      "class { 'memcached': #{puppet_options}}"
     end
   end
 end
--- spec/unit/modules/memcached_spec.rb
+++ spec/unit/modules/memcached_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe Vagrant::Boxen::Modules::Memcached do
   let(:manifest) { subject.build_manifest }
 
+  subject { described_class.new(:memory => '10%') }
+
   it 'includes memcached class on manifest' do
     manifest.should =~ /^class { 'memcached': /
+  end
+
+  it 'maps memory option to max_memory' do
+    manifest.should =~ /max_memory\s+=>\s+'10%',/
   end
 end
